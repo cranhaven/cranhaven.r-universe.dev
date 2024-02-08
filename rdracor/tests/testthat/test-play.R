8984e133d823ff3e77f7cbad3ed04650248a8622
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-02-08 02:02:00 +0000

    Add rdracor to CRANhaven, because archived on 2024-02-08 02:02:00 +0000

---
+++ rdracor/tests/testthat/test-play.R
@@ -0,0 +1,13 @@
+test_that("get_play_metadata returns list", {
+  expect_type(get_play_metadata("pushkin-boris-godunov", "rus"), "list")
+})
+
+test_that("rdf is not empty", {
+  tei <- get_play_rdf("pushkin-boris-godunov", "rus")
+  text <- xml2::xml_text(tei)
+  expect_gt(nchar(text), 0)
+})
+
+test_that("boris godunov cast has 79 characters", {
+  expect_equal(nrow(get_play_cast("pushkin-boris-godunov", "rus")), 79)
+})
