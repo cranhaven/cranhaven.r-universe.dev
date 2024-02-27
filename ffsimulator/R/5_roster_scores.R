037321e7cb4cfe7acec9bb9713b25592f343237f
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-02-27 06:02:00 +0000

    Add ffsimulator to CRANhaven, because archived on 2024-02-27 06:02:00 +0000

---
+++ ffsimulator/R/5_roster_scores.R
@@ -0,0 +1,57 @@
+#' Join Rosters to Projected Scores
+#'
+#' Attaches projected scores to rosters (via an inner-join) and creates a positional ranking column.
+#'
+#' @param projected_scores a dataframe of projected scores, as created by `ffs_generate_projections()`
+#' @param rosters a dataframe of rosters, as created by `ffs_rosters()`
+#'
+#' @examples \donttest{
+#' # cached examples
+#' projected_scores <- .ffs_cache("projected_scores.rds")
+#' rosters <- .ffs_cache("mfl_rosters.rds")
+#'
+#' ffs_score_rosters(projected_scores, rosters)
+#' }
+#'
+#' @seealso vignette("custom") for example usage
+#'
+#' @return A dataframe of roster-level projected scores
+#'
+#' @export
+ffs_score_rosters <- function(projected_scores, rosters) {
+  checkmate::assert_data_frame(projected_scores)
+  checkmate::assert_data_frame(rosters)
+
+  assert_columns(
+    projected_scores,
+    c(
+      "fantasypros_id", "ecr", "rank", "projection",
+      "gp_model", "season", "week",
+      "projected_score", "scrape_date"
+    )
+  )
+
+  assert_columns(
+    rosters,
+    c("fantasypros_id", "league_id", "franchise_id", "pos")
+  )
+
+  projected_scores <- data.table::as.data.table(
+    projected_scores[, c(
+      "fantasypros_id", "ecr", "rank", "projection", "gp_model",
+      "season", "week", "projected_score", "scrape_date"
+    )]
+  )
+
+  data.table::setDT(rosters)
+  data.table::setkeyv(projected_scores, "fantasypros_id")
+  data.table::setkeyv(rosters, "fantasypros_id")
+
+  roster_scores <- merge(rosters, projected_scores, by = "fantasypros_id", all = FALSE, allow.cartesian = TRUE)
+
+  roster_scores[order(-roster_scores$projected_score),
+    `:=`(pos_rank = seq_len(.N)),
+    by = c("league_id", "franchise_id", "pos", "season", "week")
+  ]
+  return(roster_scores)
+}
---
+++ ffsimulator/R/utils-data.R
@@ -0,0 +1,17 @@
+#' FP injury table
+#'
+#' This dataframe contains a column (`prob_gp`) for each positional ranking that describes the probability of a player with that preseason ADP playing in a given game. It is modelled from historical rankings data and the number of games played per season for a given positional rank.
+#'
+"fp_injury_table"
+
+#' Historical draft position ranks
+#'
+#' This dataframe has historical positional draft rankings for 2012-2020 QB/RB/WR/TE/PK and 2015-2020 DL/LB/DB, as gathered by the ffpros package.
+#'
+"fp_rankings_history"
+
+#' Historical position ranks
+#'
+#' This dataframe has historical positional in-season rankings for 2012-2020 QB/RB/WR/TE/PK and 2015-2020 DL/LB/DB, as gathered by the ffpros package.
+#'
+"fp_rankings_history_week"
