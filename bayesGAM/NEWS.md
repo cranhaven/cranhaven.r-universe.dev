3dd74a33f805f7709b37bdb6f382af8d568b9ad5
GitHub Action <action@github.com>
2024-12-03 04:02:00 +0000

    Add bayesGAM to CRANhaven, because archived on 2024-12-03 04:02:00 +0000

---
+++ bayesGAM/NEWS.md
@@ -0,0 +1,5 @@
+# bayesGAM 0.0.2
+
+- Streamlined summary output of bayesGAM models
+- Updated email address: samthoma@alumni.iu.edu
+
---
+++ bayesGAM/R/imports.R
@@ -0,0 +1,10 @@
+#' @importMethodsFrom rstan plot extract summary
+#' @importFrom cluster clara
+#' @importFrom graphics abline
+#' @importFrom corrplot corrplot.mixed
+#' @importFrom boot inv.logit
+#' @import geometry
+#' @import stats
+#' @import SemiPar
+#' @import ggplot2
+NULL
