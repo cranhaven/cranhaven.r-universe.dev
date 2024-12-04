dcb4a7c9ddba3da27fb6ff04bfad6a460a92bb98
GitHub Action <action@github.com>
2024-12-04 02:02:00 +0000

    Add pfa to CRANhaven, because archived on 2024-12-04 02:02:00 +0000

---
+++ pfa/R/core.R
@@ -0,0 +1,235 @@
+#####################################
+##### core function: pfa  ##########
+#####################################
+
+pfa <- function(Z, Sigma, t, Kmax, reg="L1", e=.05, gamma, K, plot="-log") {
+  
+  Z <- as.vector(Z)
+  Sigma <- as.matrix(Sigma)
+  p <- length(Z)
+  
+  # standardize the data
+
+  SD <- sqrt(diag(Sigma))
+  Z <- Z/SD
+  Sigma <- diag(1/SD) %*% Sigma %*% diag(1/SD)
+
+  # pca 
+
+  pca <- svd(Sigma, nu=0, nv=Kmax)
+  lambda <- pca$d
+  eigvec <- pca$v
+    
+
+  # determine the factor loadings
+  
+  if (missing(K)) {
+      K = 1
+      while( K < Kmax & sqrt(sum(lambda[(K+1):length(lambda)]^2)) >= e*sum(lambda)) 
+         K = K + 1 
+  }    
+  sqrt_lambda <- as.matrix(sqrt(lambda[1:K]))
+  b <- as.matrix(eigvec[,1:K])
+  for (i in 1:K)  {
+    b[,i] <- b[,i] * sqrt_lambda[i]  # factor loadings
+  }
+
+
+  # estimate the factors, with 5% largest |Z| eliminated.
+   
+  
+  if (reg == "L1") {
+    W.hat <- rq(Z ~ b -1, 0.5)$coef     # L_1 regression (no intercept)
+    # W.hat <- W.hat[2:(K+1)] 
+  } 
+  else if (reg == "L2")  # L_2 regression (no intercept) 
+  {    
+    #temp<-sort(abs(Z),decreasing=TRUE,index.return=TRUE)
+    #len=round(length(Z)*0.05)
+    #Ztemp<-temp$x
+    #btemp<-as.matrix(b[temp$ix,])
+    #Ztemp<-Ztemp[(len+1):length(Z)]
+    #btemp<-btemp[(len+1):length(Z),]
+    #W.hat<-lm(Ztemp ~ btemp - 1)$coef
+    o = order(abs(Z))
+    Zperm = Z[o]
+    Lperm = as.matrix(b[o,])
+    Z.reduce = Zperm[1:(p*0.95)]
+    L.reduce = as.matrix(Lperm[1:(p*0.95),]) 
+    W.hat = lsfit(x=L.reduce,y=Z.reduce,intercept=F)$coef
+  }
+  #if (reg == "huber") {
+    #W.hat <- rlm(Z ~ b, 0.5)$coef	  # robust/huber regression
+  #}
+
+  rs <- rowSums(b^2)
+  inv_a <- sqrt( ((1 - rs)+abs(1-rs))/2 )
+  bW.est <- b%*%(W.hat)
+
+
+  # compute p-values & estimate p0
+  
+  P <- 2*(1-pnorm(abs(Z)))
+  sort <- sort(P, index.return=TRUE)
+  index <- sort$ix
+  P <- sort$x
+  if (missing(gamma))  
+      gamma <- as.numeric(quantile(P, probs=0.4))
+  p0.est <- min(p, sum(P>gamma)/(1-gamma))
+
+
+  # estimate FDP
+  
+  t.default <- TRUE
+  if (!missing(t)) {
+       if (t=="pval")  {
+          t <- P
+          t.default <- FALSE
+       } 
+       if (is.numeric(t)) 
+          t.default = ( sum(t>=0)+ sum(t<=1) < 2*length(t))
+  }
+  if (t.default) {
+       logt.l <- max(min(log(P)), log(1e-14))
+       logt.u <- max(log(P))
+       grid <- (logt.u-logt.l)*seq(from=0.01,to=1,by=0.025)*0.5 + 0.85*logt.l+0.15*logt.u
+       t <- exp(grid)
+  }
+       
+  FDPt <- Vt <- Rt<- rep(0, length(t))
+  for (l in 1:length(t)) {
+  	   P1 <- 2*(1-pnorm(abs(Z)))
+       Rt[l] <- sum(P1<=t[l])
+       a <- rep(0,p)
+       for (j in 1:p)  {
+           qtl <- qnorm(t[l]/2)
+           if (inv_a[j]>0)  {
+                a[j] <- pnorm((qtl + bW.est[j])/inv_a[j])+ pnorm((qtl - bW.est[j])/inv_a[j])
+           } else {
+                a[j] <- as.numeric(abs(bW.est[j])>abs(qtl))
+           }
+       }
+       Vt[l] <- min(sum(a), Rt[l]) 
+       if (Rt[l]==0)   {
+           FDPt[l] <- 0
+       } else  {
+           FDPt[l] <- Vt[l]/Rt[l]
+       }
+  } 
+
+
+  # factor adjusted procedure
+
+  adj.P <- as.vector(rep(0,p))
+  for (j in 1:p) {
+       if (inv_a[j]>0)  {
+           adj.P[j] <- 2*( 1- pnorm(abs(Z[j] - bW.est[j])/inv_a[j]) )
+       }  else  {
+           adj.P[j] <- as.numeric(abs(Z[j]-bW.est[j])==0)
+       }
+  }
+  sort <- sort(adj.P, index.return=TRUE)
+  adj.index <- sort$ix
+  adj.P <- sort$x
+
+  
+  # output
+  
+  Pvals <- data.frame(p.value = P,  Index =index)
+  adjPvals <- data.frame(p.value = adj.P,  Index = adj.index)
+  if (t.default)   {
+       FDPvals <- data.frame( minus.logt= - log(t), rejects= Rt, false.rejects=Vt, FDP=FDPt )
+  } else {
+       FDPvals <- data.frame( t= t, rejects= Rt, false.rejects=Vt, FDP=FDPt )
+  }
+  results <- list("Pvalue"=Pvals, "adjPvalue"=adjPvals, "FDP"=FDPvals, "pi0"=p0.est/p, "K"=K, "sigma"=NULL)
+  class(results) <- "FDPresults"
+  if (plot=="-log")  {
+            par(mfrow=c(2,2))
+            hist(P, main = "Histogram of p-values", xlab="p-values")
+            plot(-log(t), Rt, xlab="-log(t)", ylab="", main="Number of total rejections", type='o')
+            plot(-log(t),Vt, xlab="-log(t)", ylab="", main="Number of estimated false rejections", type='o')
+            plot(-log(t),FDPt,xlab="-log(t)", ylab="", main="Estimated FDP", type='o')
+   }  else  if (plot=="linear") {
+            par(mfrow=c(2,2))
+            hist(P, main = "Histogram of p-values", xlab="p-values")
+            plot(t, Rt, xlab="t", ylab="", main="Number of total rejections", type='o')
+            plot(t,Vt, xlab="t", ylab="", main="Number of estimated false rejections", type='o')
+            plot(t,FDPt,xlab="t", ylab="", main="Estimated FDP", type='o')
+  }  else if (plot=="log") {
+            par(mfrow=c(2,2))
+            hist(P, main = "Histogram of p-values", xlab="p-values")
+            plot(log(t), Rt, xlab="log(t)", ylab="", main="Number of total rejections", type='o')
+            plot(log(t),Vt, xlab="log(t)", ylab="", main="Number of estimated false rejections", type='o')
+            plot(log(t),FDPt,xlab="log(t)", ylab="", main="Estimated FDP", type='o')
+  }
+  return(results)
+}
+
+
+#########################################
+##### core function: print.FDPresults ###
+#########################################
+
+print.FDPresults <- function(x, ...)  {
+
+  cat("P-values:\n")
+  print(x$Pvalue)
+  
+  cat("Factor-ajusted P-values:\n")
+  print(x$adjPvalue)
+
+  cat("Estimated false discoveries:\n")
+  print(x$FDP)
+  
+  cat("Estimated true null proportion:\n")
+  print(x$pi0)
+
+  cat("Number of factors:\n")
+  print(x$K)
+
+  if (! is.null(x$sigma))  {
+      cat("Estimated noise standard deviation:\n")
+      print(x$sigma)
+  }
+}
+
+
+###########################################
+##### core function: summary.FDPresults ###
+###########################################
+
+summary.FDPresults <- function(object, ...)  { 
+  x <- object
+
+  cat("Method:\n")
+  cat(paste("PFA with", x$K, "factors.\n"))
+  cat("\n")
+  
+  p <- length(x$Pvalue$p.value)
+  cat("P-values:\n")
+  print(x$Pvalue[1:min(20,p),])
+  cat("...\n")
+  
+  cat("Factor-ajusted P-values:\n")
+  print(x$adjPvalue[1:min(20,p),])
+  cat("...\n")
+
+  T <- length(x$FDP$rejects)
+  cat("Estimated false discoveries:\n")
+  print(x$FDP[1:min(10,T),])
+  cat("...\n")
+  
+  cat("Estimated true null proportion:\n")
+  cat(paste(round(x$pi0, digits=3), "\n"))
+
+  if (! is.null(x$sigma))  {
+      cat("Estimated noise standard deviation:\n")
+      cat(paste(round(x$sigma,digits=3), "\n"))
+  }
+}  
+
+
+
+
+
---
+++ pfa/R/use.R
@@ -0,0 +1,252 @@
+require(quantreg)
+require(lars)
+require(POET)
+
+
+#####################################
+##### user function 1: pfa.test #####
+#####################################
+
+pfa.test <- function(X, Y, tval, Sigma, reg="L2", K, e=0.05, gamma, mat_est="poet", plot="-log") {
+
+
+# compute the test statistics
+
+if (missing(Y)) {
+    X <- as.matrix(X)
+    if (ncol(X)==1)  {
+      Z <- as.vector(X)
+    }  else  {
+      Z <- colMeans(X)*sqrt(nrow(X))
+    }
+} else {
+    X <- as.matrix(X)
+    Y <- as.matrix(Y)
+    if (ncol(X)!= ncol(Y))
+      stop("Dimensions of X and Y do not match.")
+    Z <- (colMeans(X) - colMeans(Y))*sqrt(nrow(X)*nrow(Y)/(nrow(X)+nrow(Y)))
+}
+p <- length(Z)
+Kmax <- p
+
+# determine number of factors
+if (missing(K)&(ncol(X)>=2)) {
+	if(missing(Y)){
+	  kmax<-floor(0.2*nrow(X))
+	  if(kmax<=1){
+		stop("No enough samples to determine the number of factors!")
+	   }else{
+      sample.cor<-cor(t(X),t(X))
+      sample.pca<-eigen(sample.cor)
+      sample.eigval<-sample.pca$values
+      ratio<-sample.eigval[1:(kmax-1)]/sample.eigval[2:kmax]
+      K<-which(ratio==max(ratio))	
+       }
+    }else{
+    	  n<-nrow(X)
+    	  m<-nrow(Y)
+    	  kmax<-floor(0.2*(n+m))
+    	  if(kmax<=1){
+    	  	stop("No enough samples to determine the number of factors!")
+    	  }else{
+    	    Xmean<-apply(X,2,mean)
+    	    Ymean<-apply(Y,2,mean)
+    	    Znew<-as.matrix(rbind(X-Xmean,Y-Ymean))
+    	    sample.cor<-cor(t(Znew),t(Znew))
+        sample.pca<-eigen(sample.cor)
+        sample.eigval<-sample.pca$values
+        ratio<-sample.eigval[1:(kmax-1)]/sample.eigval[2:kmax]
+        K<-which(ratio==max(ratio))
+      }
+    }
+}
+
+
+# compute the covariance matrix
+
+if (missing(Sigma)) {
+    if (missing(Y) & nrow(X)==1) {
+       stop("No enough samples to estimate the covariance matrix.")
+    } 
+    if (mat_est =="sample") {
+       n <- nrow(X)
+       barX <- matrix(colMeans(X), nrow=n, ncol=p, byrow=T)
+       SigmaX <- (t(X - barX )%*% (X - barX))/n
+       if (missing(Y)) {
+          Sigma <- SigmaX
+          Kmax <- n
+       } else {
+          n2 <- nrow(Y)
+          barY <- matrix(colMeans(Y), nrow=n2, ncol=p, byrow=T)
+          SigmaY <- (t(Y - barY)%*% (Y - barY))/n2
+          Sigma <- (SigmaX*n + SigmaY*n2)/(n+n2)
+          Kmax <- n + n2
+       }  
+     } else { 
+          if (mat_est=="poet") {
+             n <- nrow(X)
+             barX <- matrix(colMeans(X), nrow=n, ncol=p, byrow=T)
+             if (missing(Y)) {
+                # R <- X - barX
+                R<-X
+             } else {
+                n2 <- nrow(Y)
+                barY <- matrix(colMeans(Y), nrow=n2, ncol=p, byrow=T)
+                # R <- rbind(X-barX, Y-barY)
+                R<-rbind(X,Y)
+             }
+             Sigma <- POET(t(R),K=K,C=0.5,thres='soft',matrix='vad')$SigmaY
+             Kmax <- p
+          }
+     }
+}
+
+# run the test and compute FDP
+
+results <- pfa(Z=Z, Sigma=Sigma, t=tval, Kmax=Kmax, reg=reg, e=e, gamma=gamma, K=K, plot=plot)
+return(results)
+
+}
+
+
+#####################################
+##### user function 2: pfa.gwas #####
+#####################################
+
+
+pfa.gwas <- function(X,Y, tval, v, reg="L1", e=0.05, gamma, K, plot="-log") {
+
+    X <- as.matrix(X)
+    Y <- as.matrix(Y)
+    if (nrow(X)!=nrow(Y) | ncol(Y)!=1) 
+      stop("Dimensions do not match.")
+    n <- nrow(X)
+    p <- ncol(X)
+    
+    # compute Z and Sigma
+
+    Z <- rep(0, p)
+    for (j in 1:p)  {    # marginal regression
+        Z[j] <- lm(Y ~ X[,j])$coef[2]
+    }
+    if (missing(v)) {
+        sigma.est <- rcv(X,Y)     # refitted cross validation
+    }  else {
+        sigma.est <- v
+    }
+    barX <- matrix(colMeans(X), nrow=n, ncol=p, byrow=T)
+    R <- (t(X-barX)%*% (X-barX))/n 
+    Sigma <- matrix(rep(0, p*p), nrow=p)
+    for (i in 1:p) {
+        for (j in i:p)  {
+             Sigma[i,j] <- Sigma[j,i] <- R[i,j]*(sigma.est)^2/R[i,i]/R[j,j]/n
+        }
+    }
+    
+    # run the test and compute FDP
+    
+    results <- pfa(Z, Sigma, t=tval, Kmax=n, reg=reg, e=e, gamma=gamma, K=K, plot=plot)
+    results$Pvalue <- data.frame(coefficient = Z[results$Pvalue$Index], p.value = results$Pvalue$p.value,  Index =results$Pvalue$Index)
+    results$sigma <- sigma.est
+    return(results)
+}
+
+
+
+# refitted cross validation
+
+rcv <- function(x, y) {
+  n <- nrow(x)
+  p <- ncol(x)
+  # dimension reduction by removing ultra-highly correlated variables
+  # C <- abs(cor(x,x)) 
+  # correlation matrix
+  
+  barx<-colMeans(x)
+  C<-t(x - barx)%*% (x - barx)/(n-1)
+  for (i in 1:p) {
+     C[i,] <- C[i,]/sqrt(C[i,i])
+    }
+    for (j in 1:p) {
+     C[,j] <- C[,j]/sqrt(C[j,j])
+    }
+  C <- C*upper.tri(C) # all values under diagonal to 0
+  if(nrow(C) > 0) {
+  	if(sum(C>0.98)!=0){
+    cols <- which(C > 0.98, arr.ind=T)
+    cols <- unique(cols[,2])
+    x <- x[,-cols]
+    }
+  }
+  sigma_sq <- sigma1_sq <- sigma2_sq <- rep(0,30)
+  for (i in 1:30){
+    count <-1
+    repeat {
+      seq <- c(1:n)
+      seq <- sample(seq)
+      seq1 <- seq[1:(n/2)]
+      seq2 <- seq[((n/2)+1):n]	# random split of the data into two halves
+      y1 <- y[seq1]
+      X1 <- x[seq1,]
+      y2 <- y[seq2]
+      X2 <- x[seq2,]
+      try(result1<-lars(X1,y1,type="lasso", normalize=FALSE, use.Gram=FALSE),silent=TRUE)# LASSO algorithm
+      try(result2<-lars(X2,y2,type="lasso", normalize=FALSE, use.Gram=FALSE),silent=TRUE)# LASSO algorithm
+      if(exists("result1") & exists("result2"))
+        break
+      count <- count+1
+      if(count>1000)
+        stop("Refitted cross validation fails. Please input v.")
+    }
+
+    # refitted cross-validation (RCV)
+    if(p<200){
+       cvres1 <- cv.lars(X1, y1, K=5,plot.it=FALSE,type="lasso")	  
+       sBest1<-cvres1$index[which.min(cvres1$cv)]
+       cres1<-predict.lars(result1,type="coefficients",s=sBest1,mode="fraction")
+       c1<-cres1$coefficient
+    } else{
+       c1<-coef(result1)[10,]
+    }
+    if(p<200){
+       cvres2 <- cv.lars(X2, y2, K=5, plot.it=FALSE,type="lasso")	  
+       sBest2<-cvres1$index[which.min(cvres2$cv)]
+       cres2<-predict.lars(result2,type="coefficients",s=sBest2,mode="fraction")
+       c2<-cres2$coefficient
+    } else{
+       c2<-coef(result2)[10,]
+    }
+      
+    
+    ind1<-which(c1!=0)
+    if (length(ind1)!=0){
+    #ind1 <- (c1[10,]!=0)	# specifying model size
+    resid1 <- lsfit(X2[,ind1],y2)$resid
+    sigma1_sq[i] <- sum(resid1^2)/(n/2 - length(ind1))
+    }   else{
+    sigma1_sq[i] <- sum(y2^2)/(n/2) }
+    
+    
+    #ind2 <- (c2[10,]!=0)
+    
+    ind2<-which(c2!=0)
+    if(length(ind2)!=0){
+    resid2 <- lsfit(X1[,ind2],y1)$resid
+    sigma2_sq[i] <- sum(resid2^2)/(n/2 - length(ind2))
+    }  else{
+    	sigma2_sq[i] <- sum(y1^2)/(n/2)
+    	}
+    sigma_sq[i] <- (sigma1_sq[i] + sigma2_sq[i])/2	   # RCV-estimated variance
+  }
+  sigmahat=sqrt(mean(sigma_sq[sigma_sq!=0]))
+  return(sigmahat)
+}
+
+
+  
+
+
+
+
+
+
