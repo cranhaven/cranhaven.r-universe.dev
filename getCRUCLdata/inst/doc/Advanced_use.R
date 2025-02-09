5d2630b0c27713485a6fb3650cf94183ce76d2f3
GitHub Action <action@github.com>
2025-02-09 11:02:00 +0000

    Add getCRUCLdata to CRANhaven, because archived on 2025-02-09 11:02:00 +0000

---
+++ getCRUCLdata/inst/doc/Advanced_use.R
@@ -0,0 +1,16 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval=FALSE---------------------------------------------------------------
+# tmn_tmx <- get_CRU_stack(
+#   tmn = TRUE,
+#   tmx = TRUE,
+#   cache = TRUE
+# )
+
+## ----eval=FALSE---------------------------------------------------------------
+# t <- create_CRU_df(tmp = TRUE, dsn = "~/Downloads")
+
---
+++ getCRUCLdata/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(getCRUCLdata)
+
+test_check("getCRUCLdata")
