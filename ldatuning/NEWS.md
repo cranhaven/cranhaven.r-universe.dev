ed60ba42d58f6657abb626c97a892778985e0f36
GitHub Action <action@github.com>
2025-04-27 04:02:00 +0000

    Add ldatuning to CRANhaven, because archived on 2025-04-27 04:02:00 +0000

---
+++ ldatuning/NEWS.md
@@ -0,0 +1,10 @@
+# ldatuning 1.0.2
+  * Fixed bug introduced by return_models option in FindTopicNumbers_plot(#16)
+
+# ldatuning 1.0.1
+  * Option to return list of trained models (#2)
+  * Expose scoring algorithms to public namespace (#15, @titaniumtroop)
+  
+# ldatuning 1.0.0
+  * Load balancing (#5) implemented by calculating topics in descending order
+  * Parallel processing for multi-node clusters (#12, @titaniumtroop)
---
+++ ldatuning/R/ldatuning.R
@@ -0,0 +1,24 @@
+#' ldatuning: Tuning of the LDA models parameters
+#'
+#' @docType package
+#' @name ldatuning
+#' @description A package for identifying the number of topics in a text corpus
+#' by generating LDA models, tuning LDA model parameters, and scoring model
+#' results.
+NULL
+#> NULL
+
+# ### common
+# devtools::use_package("knitr",       "Suggests")
+#
+# ### metrics
+# devtools::use_package("parallel",    "Imports")
+# devtools::use_package("topicmodels", "Imports")
+# devtools::use_package("slam",        "Imports")
+# devtools::use_package("Rmpfr",       "Imports")
+#
+# ### plots
+# devtools::use_package("ggplot2",     "Imports")
+# devtools::use_package("reshape2",    "Imports")
+# devtools::use_package("scales",      "Imports")
+# devtools::use_package("grid",        "Imports")
