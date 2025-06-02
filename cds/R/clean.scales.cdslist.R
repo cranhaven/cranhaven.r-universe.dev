c4b8e94950e09087d45abd2366007d78848a8f98
GitHub Action <action@github.com>
2025-06-02 05:02:00 +0000

    Add cds to CRANhaven, because archived on 2025-06-02 05:02:00 +0000

---
+++ cds/R/clean.scales.cdslist.R
@@ -0,0 +1,8 @@
+#' @method clean.scales cdslist
+#' @rdname clean.scales
+#' @export
+clean.scales.cdslist <- function(object, data, K, col.subset = NULL, ...){
+  Kvec <- lapply(object, "[[", "K")
+  object <- object[[which(Kvec == K)]]
+  return(clean.scales(object, data, col.subset, ...))
+}
