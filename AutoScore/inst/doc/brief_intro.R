e411c342920393fde655397896e99f2254332f73
GitHub Action <action@github.com>
2025-07-10 11:02:00 +0000

    Add AutoScore to CRANhaven, because archived on 2025-07-10 11:02:00 +0000

---
+++ AutoScore/inst/doc/brief_intro.R
@@ -0,0 +1,10 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  echo = TRUE,
+  collapse = TRUE,
+  comment = "#>",
+  cache = FALSE
+)
+
+options(rmarkdown.html_vignette.check_title = FALSE)
+
