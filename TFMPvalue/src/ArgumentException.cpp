2927783353b439439fb876561bb06fd95f8ef9a6
GitHub Action <action@github.com>
2025-12-25 04:02:00 +0000

    Add TFMPvalue to CRANhaven, because archived on 2025-12-25 04:02:00 +0000

---
+++ TFMPvalue/src/ArgumentException.cpp
@@ -0,0 +1,11 @@
+/*
+ *  ArgumentException.cpp
+ *  pvalue
+ *
+ *  Created by Jean-Stphane Varr on 02/07/07.
+ *  Copyright 2007 LIFL-USTL-INRIA. All rights reserved.
+ *
+ */
+
+#include "ArgumentException.h"
+
