57c6db9cfb46af82b5f51f1d6e3cb162ff26fb0f
GitHub Action <action@github.com>
2026-01-18 05:02:00 +0000

    Add ascentTraining to CRANhaven, because archived on 2026-01-18 05:02:00 +0000

---
+++ ascentTraining/R/ascentTraining-package.R
@@ -0,0 +1,16 @@
+#' @name ascentTraining-package
+#' @title Ascent Training Datasets
+#' @aliases ascentTraining ascentTraining-package
+#' @description Datasets designed to be used in conjunction with Ascent training materials.
+#' @docType package
+#' @details 
+#' Datasets designed to be used in conjunction with Ascent training materials and 
+#' book, SAMS Teach Yourself R in 24 Hours (ISBN: 978-0-672-33848-9). The data covers a range 
+#' of applications and has been collected together from a number of sources.
+#' The airquality dataset, from the Core R datasets package is also provided in xlsx format
+#' in the extdata directory of this package. 
+#' @author Ascent
+#'
+#' Contact: Ascent \email{rin24hours@@mango-solutions.com}
+NULL
+
---
+++ ascentTraining/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ascentTraining)
+
+test_check("ascentTraining")
