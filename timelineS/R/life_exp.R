b715b3185afb8d28a30d3e0132fc4466c3fc31a8
GitHub Action <action@github.com>
2026-03-18 05:02:00 +0000

    Add timelineS to CRANhaven, because archived on 2026-03-18 05:02:00 +0000

---
+++ timelineS/R/life_exp.R
@@ -0,0 +1,7 @@
+#' @title Data for examples in timelineS package
+#' @description Dates of birth and death, country, gender and names
+#' @name life_exp
+#' @docType data
+#' @format dataframe of name(character), country(character), gender(character), and dates(date)
+#' @keywords durSummary, durPlot, durCalc
+NULL
---
+++ timelineS/R/mj_life.R
@@ -0,0 +1,6 @@
+#' @title Data for timelineS function example in timelineS package
+#' @description Events and dates of Michael Jackson's life
+#' @name mj_life
+#' @docType data
+#' @format dataframe of events(character) and dates(date)
+NULL
